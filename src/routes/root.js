ce1735fbda5f6cbcb8de6956de2a0e2359d91a03
Louis Borsu <sat@satprod.net>
2018-07-10 14:49:33 +0200

    fix(website): update versions

--- src/routes/root.js
+++ src/routes/root.js
@@ -4,11 +4,14 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/*',function(req,res,next){
-    res.header('Content-Security-Policy' , 'default-src https: data: \'unsafe-inline\' \'unsafe-eval\'');
+  if (!process.env.LOCAL_DEV) {
+    res.header('Content-Security-Policy' , 'Content-Security-Policy: default-src https://satprod.net:443');
+    res.header('Referrer-Policy' , 'same-origin');
     res.header('X-Frame-Options' , 'SAMEORIGIN');
     res.header('X-Xss-Protection' , '1; mode=block');
     res.header('X-Content-Type-Options' , 'nosniff');
-    next();
+  }
+  next();
 });
 
 router.get('/*', function (req, res) {
